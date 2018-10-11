cda0cbe60330ff2b6ab51e679589afcf97c14232
Hugo-mesk <hugo.mesk@gmail.com>
2018-10-10 22:08:58 -0300

    correcting settings

--- economia_rio/settings.py
+++ economia_rio/settings.py
@@ -52,14 +52,18 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(DATA_DIR, 'media')
-STATIC_ROOT = os.path.join(DATA_DIR, 'static')
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/static/'
 
+# Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'economia_rio', 'static'),
+    os.path.join(BASE_DIR, 'static'),
 )
+
 SITE_ID = 1
 
 
